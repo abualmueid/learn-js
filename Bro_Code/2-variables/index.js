526acfa51ac85d9f207db2de2b4eceba5d30a53e
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2024-11-23 12:48:24 +0600

    Create file

---
+++ Bro_Code/2-variables/index.js
@@ -0,0 +1,11 @@
+let name = "Mueid";
+let age = 30;
+
+console.log(`My name is ${name}`);
+console.log(`My age is ${age}`);
+
+let cgpa = 3.33;
+let major = "CS";
+
+document.getElementById("cgpa").textContent = `My cgpa is ${cgpa}`;
+document.getElementById("major").textContent = `My major is ${major}`;
