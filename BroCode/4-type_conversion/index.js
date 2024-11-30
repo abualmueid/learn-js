381b6ca50dadd0314ae706830b2fffa1f1019f0e
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2024-11-30 18:10:53 +0600

    Add file

---
+++ BroCode/4-type_conversion/index.js
@@ -0,0 +1,13 @@
+let x = "10";
+let y = true;
+let z = 20;
+
+console.log(x, typeof x);
+
+x = Number(x);
+y = String(y);
+z = Boolean(20);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
