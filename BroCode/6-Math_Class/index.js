277acbf47469aa55bfce91fb3e69e33b7139bd96
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2024-11-30 21:12:28 +0600

    Add file

---
+++ BroCode/6-Math_Class/index.js
@@ -0,0 +1,16 @@
+let x = 2.5;
+let y = -3;
+let z = 4;
+
+console.log(Math.PI);
+console.log(Math.floor(x));
+console.log(Math.ceil(x));
+console.log(Math.trunc(x));
+console.log(Math.log(x));
+console.log(Math.sin(x));
+console.log(Math.cos(x));
+console.log(Math.tan(x));
+console.log(Math.abs(y));
+console.log(Math.sign(y));
+console.log(Math.max(x, y, z));
+console.log(Math.min(x, y, z));
