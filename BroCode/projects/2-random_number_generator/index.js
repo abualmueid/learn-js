41710e59ceb4981c9d0a4edfc569d21d4853c394
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2024-11-30 22:06:02 +0600

    Add file

---
+++ BroCode/projects/2-random_number_generator/index.js
@@ -0,0 +1,18 @@
+let label1 = document.getElementById("label1");
+let label2 = document.getElementById("label2");
+let label3 = document.getElementById("label3");
+let rollBtn = document.getElementById("rollBtn");
+
+let min = 1; 
+let max = 6
+let randomNum;
+
+rollBtn.onclick = function () {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
