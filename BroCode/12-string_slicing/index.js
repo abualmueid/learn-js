b69f9750fdd2ba5a122ebd39150a23289de0d9e3
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2024-12-07 21:05:55 +0600

    Add file

---
+++ BroCode/12-string_slicing/index.js
@@ -0,0 +1,6 @@
+let email = "abualmueid24@gmail.com";
+let username = email.slice(0, email.indexOf('@'));
+let extension = email.slice(email.indexOf('@') + 1);
+
+console.log(username);
+console.log(extension);
