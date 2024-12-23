c8cbeb3edeb4729c7ecd2ce60562827d4101c25c
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2024-12-23 15:35:45 +0600

    Add file

---
+++ BroCode/projects/6-random_password_generator/index.js
@@ -0,0 +1,31 @@
+function generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = lowercaseChars.toUpperCase();
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-=";
+
+    let allowedChars = "";
+    let password = "";   
+    
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    for(let i = 0; i < passwordLength; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex]; 
+    }
+    
+    return password;
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(password);
