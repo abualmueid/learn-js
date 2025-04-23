dbc3c4c99d9d3b9a8857a938b2b86bbb4a8263c3
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-04-23 23:01:40 +0600

    Modify file

--- BroCode/33-Date_object/index.js
+++ BroCode/33-Date_object/index.js
@@ -1,13 +1,37 @@
-people = [
-    {name: "Mueid", age: 30, gpa: 3.33},
-    {name: "Masud", age: 40, gpa: 4.00},
-    {name: "Rakib", age: 27, gpa: 3.50},
-    {name: "Rasel", age: 37, gpa: 2.33},
-    {name: "Sajjad", age: 33, gpa: 1.79},
-];
+date = new Date();
+console.log(date);
 
-people.sort((a, b) => a.age - b.age);
-console.log(people);
+date = new Date(0);
+console.log(date);
 
-people.sort((a, b) => a.name.localeCompare(b.name));
-console.log(people);
+date = new Date(2025, 3, 23, 22, 28, 25);
+console.log(date);
+
+date = new Date();
+
+year = date.getFullYear();
+month = date.getMonth();
+day = date.getDate();
+hour = date.getHours();
+minute = date.getMinutes();
+second = date.getSeconds();
+dayOfWeek = date.getDay();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+console.log(minute);
+console.log(second);
+console.log(dayOfWeek);
+
+date = new Date();
+
+date.setFullYear(2026);
+date.setMonth(0);
+date.setDate(10);
+date.setHours(6);
+date.setMinutes(10);
+date.setSeconds(5);
+
+console.log(date);
